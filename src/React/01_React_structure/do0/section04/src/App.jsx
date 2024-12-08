a95917d8f7a19275994da1d81a42f51c2566b967
SorrowAddict <soantine789@naver.com>
2024-12-09 03:13:14 +0900

    chore: Updated settings files

--- src/React/01_React_structure/do0/section04/src/App.jsx
+++ src/React/01_React_structure/do0/section04/src/App.jsx
@@ -2,13 +2,24 @@ import './App.css'
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import Button from './components/Button'
 
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  }
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <div>자식요소</div>
+      </Button>
     </>
   )
 }
