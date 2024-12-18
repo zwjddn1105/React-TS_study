94c6faeba1034c1dd2ed220eb36c2f0c4d8d8601
aaaange <ayung00@naver.com>
2024-12-18 18:14:26 +0900

    docs(README.md): FE Study week 2 Review

---
+++ src/React/PJT2_TDL/aaange/TDL/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Editor from './components/Editor'
+import Header from './components/Header'
+import List from './components/List'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  )
+}
+
+export default App
