a941e801cde1c8faa4d464d9f72e42aaaebf9673
kimjeongwoo <zwjddn1105@naver.com>
2025-01-14 14:45:02 +0900

    ~ts-react_2

---
+++ src/TypeScript/KJW/ts-react/lecture/client.tsx
@@ -0,0 +1,7 @@
+// export default가 아닐 때 * 사용
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import GuGuDan from "./GuGuDan";
+
+ReactDOM.render(<GuGuDan />, document.querySelector("#root"));
