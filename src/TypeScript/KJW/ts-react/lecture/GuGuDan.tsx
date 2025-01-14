3a5b614081a8eca2f2c444350413fda2532ff3bf
kimjeongwoo <zwjddn1105@naver.com>
2025-01-14 17:58:39 +0900

    6시임시저장

--- src/TypeScript/KJW/ts-react/lecture/GuGuDan.tsx
+++ src/TypeScript/KJW/ts-react/lecture/GuGuDan.tsx
@@ -1,1 +1,44 @@
+import * as React from "react";
+// import { useState } from "react";
+// <> === React.Fragment
+const GuGuDan = () => {
+  const [first, setFirst] = React.useState(Math.ceil(Math.random() * 9));
+  const [second, setSecond] = React.useState(Math.ceil(Math.random() * 9));
+  const [value, setValue] = React.useState("");
+  const [result, setResult] = React.useState("");
+  const inputEl = React.useRef(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    const input = inputEl.current;
+    if (parseInt(value) === first * second) {
+      setResult("정답");
+      setFirst(Math.ceil(Math.random() * 9));
+      setSecond(Math.ceil(Math.random() * 9));
+      setValue("");
+
+      input.focus();
+    } else {
+      setResult("땡");
+      setValue("");
+      input.focus();
+    }
+  };
+  return (
+    <>
+      <div>
+        {first} 곱하기 {second}는?
+      </div>
+      <form onSubmit={onSubmitForm}>
+        <input
+          ref={inputEl}
+          type="number"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+    </>
+  );
+};
+
 export default GuGuDan;
