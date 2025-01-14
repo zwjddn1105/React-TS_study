34593af3b9d2be1623ce59cb44f8bf2e4b030363
zwjddn1105 <zwjddn1105@naver.com>
2025-01-14 19:30:58 +0900

    아 외않되

--- src/TypeScript/KJW/ts-react/lecture/GuGuDan.tsx
+++ src/TypeScript/KJW/ts-react/lecture/GuGuDan.tsx
@@ -1,44 +1,42 @@
-import * as React from "react";
-// import { useState } from "react";
+import * as React from "react"
+import { useState, useRef } from "react"
 // <> === React.Fragment
 const GuGuDan = () => {
-  const [first, setFirst] = React.useState(Math.ceil(Math.random() * 9));
-  const [second, setSecond] = React.useState(Math.ceil(Math.random() * 9));
-  const [value, setValue] = React.useState("");
-  const [result, setResult] = React.useState("");
-  const inputEl = React.useRef(null);
+  const [first, setFirst] = useState(Math.ceil(Math.random() * 9))
+  const [second, setSecond] = useState(Math.ceil(Math.random() * 9))
+  const [value, setValue] = useState("")
+  const [result, setResult] = useState("")
+  const inputEl = useRef<HTMLInputElement>(null)
 
-  const onSubmitForm = (e) => {
-    e.preventDefault();
-    const input = inputEl.current;
+  const onSubmitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const input = inputEl.current
     if (parseInt(value) === first * second) {
-      setResult("정답");
-      setFirst(Math.ceil(Math.random() * 9));
-      setSecond(Math.ceil(Math.random() * 9));
-      setValue("");
-
-      input.focus();
+      setResult("정답")
+      setFirst(Math.ceil(Math.random() * 9))
+      setSecond(Math.ceil(Math.random() * 9))
+      setValue("")
+      if (input) {
+        input.focus()
+      }
     } else {
-      setResult("땡");
-      setValue("");
-      input.focus();
+      setResult("땡")
+      setValue("")
+      if (input) {
+        input.focus()
+      }
     }
-  };
+  }
   return (
     <>
       <div>
         {first} 곱하기 {second}는?
       </div>
       <form onSubmit={onSubmitForm}>
-        <input
-          ref={inputEl}
-          type="number"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
+        <input ref={inputEl} type="number" value={value} onChange={(e) => setValue(e.target.value)} />
       </form>
     </>
-  );
-};
+  )
+}
 
-export default GuGuDan;
+export default GuGuDan
